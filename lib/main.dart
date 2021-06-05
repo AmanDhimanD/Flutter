0c23031be753cf3cf52393786a6d49df7e28ba4e
DhimanAmanD <dhiman.1999.aman@gmail.com>
2021-06-05 22:07:13 +0530

    Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to My App."),
+          ),
+        )
+        ,),
+    );
+  }
+}
